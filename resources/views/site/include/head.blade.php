96967f82442b82621516bc3f0a5223bd94fee329
ammaraljunaid <up120161676@gmail.com>
2023-06-05 14:52:47 +0300

    first

---
+++ resources/views/site/include/head.blade.php
@@ -0,0 +1,21 @@
+
+	<!-- Basic Page Needs -->
+	<meta charset="utf-8">
+	<title>Ask me – Responsive Questions and Answers Template</title>
+	<meta name="description" content="Ask me Responsive Questions and Answers Template">
+	<meta name="author" content="2code.info">
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Main Style -->
+	<link rel="stylesheet" href="{{ asset('asset/style.css') }}">
+
+	<!-- Skins -->
+	<link rel="stylesheet" href="{{ asset('asset/css/skins/skins.css') }}">
+
+	<!-- Responsive Style -->
+	<link rel="stylesheet" href="{{ asset('asset/css/responsive.css') }}">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="{{ asset('asset/images/favicon.png') }}">
